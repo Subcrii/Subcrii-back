b0132a776de48fc7620b7e03ed3ee5f57718d205
jihye <jihye07201@gmail.com>
2025-12-02 19:48:39 +0900

    feat: 엔티티 생성
    creator, category, member, refreshtoken, favorites 엔티티 생성

---
+++ src/main/java/com/subcrii/subcrii/domain/creator/entity/Category.java
@@ -0,0 +1,19 @@
+package com.subcrii.subcrii.domain.creator.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.UUID;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private UUID parentId;
+
+    private String name;
+
+}
---
+++ src/main/java/com/subcrii/subcrii/domain/creator/entity/Creator.java
@@ -0,0 +1,46 @@
+package com.subcrii.subcrii.domain.creator.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+public class Creator {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotNull
+    private String nickName;
+
+    @Lob
+    @NotNull
+    private String information;
+
+    @NotNull
+    private Integer subscriptionPrice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @CreationTimestamp
+    private LocalDateTime created_at;
+
+    @UpdateTimestamp
+    private LocalDateTime updated_at;
+
+    private Boolean deleted = false;
+
+}
