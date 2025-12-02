3967aa66894c75a671b26592e3d364a54f672809
woojin <ooojin523@gmail.com>
2025-12-02 17:59:56 +0900

    first setting

---
+++ src/main/java/com/subcrii/subcrii/SubcriiApplication.java
@@ -0,0 +1,13 @@
+package com.subcrii.subcrii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubcriiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubcriiApplication.class, args);
+	}
+
+}
