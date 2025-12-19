c5564555641d9e94c7fb97c8cc5934fb268f1860
woojin <ooojin523@gmail.com>
2025-12-19 18:08:22 +0900

    feat: 컨텐츠 목록 조회 DTO

---
+++ src/main/java/com/subcrii/subcrii/domain/content/dto/ContentListResponse.java
@@ -0,0 +1,19 @@
+package com.subcrii.subcrii.domain.content.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@AllArgsConstructor
+@Getter
+public class ContentListResponse {
+    private UUID id;
+    private UUID creatorId;
+    private String creatorNickName;
+    private String categoryName;
+    private String title;
+    private String summary; //본문 내용 요약
+    private LocalDateTime createdAt;
+}
